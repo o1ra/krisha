4bc26d411fac32968d12475fa64a08e64803d600
Irina Kirillova <{ID}+{username}@users.noreply.github.com>
2024-01-14 00:58:25 +0600

    delete file from utils

--- krisha_kz/utils/path_env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def get_env_path(env):
-    main_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    path_env = os.path.join(main_path, 'tests', 'ui', f'.env.{env}')
-    return path_env
-
